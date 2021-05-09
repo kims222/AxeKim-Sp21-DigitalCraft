fe88565689cf0f2f4794e2704a3bdad958aec942
kims222 <kims222@newschool.edu>
2021-05-10 08:52:07 +0900

    Exploring Websites

---
+++ js/p5demosketch.js
@@ -0,0 +1,53 @@
+// runs once and setsup the scene
+function setup(){
+	createCanvas(640, 376);
+
+}
+
+// runs FOREVER
+function draw(){
+	var rectWidth = 50;
+	var rectHeight = 50;
+	background("#dad6d5");
+	noStroke();
+	fill("#2d2c31")
+	rect(50, 20, 100, 190,);
+	noStroke();
+	fill("#375270");
+	rect(5, 210, 60, 150);
+	noStroke();
+	fill("#c2a44c");
+	rect(216, 58, 200, 200);
+	fill("#375270");
+	rect(185, 20, 335, 20);
+	noStroke();
+	fill("#934a37")
+	rect(167, 228, 30, 50)
+	noStroke();
+	fill("#466756");
+	rect(98, 297, 180, 60);
+	noStroke();
+	fill("black")
+	rect(298, 276, 42, 80);
+	noStroke();
+	fill("#466756");
+	rect(361, 297, 60, 60);
+	noStroke();
+	fill("#466756")
+	rect(470, 297, 30, 60);
+	noStroke();
+	fill("black")
+	rect(470, 78, 54, 200);
+	noStroke();
+	fill("#466756");
+	rect(540, 20, 74, 171);
+	noStroke();
+	fill("#375270");
+	rect(540, 206, 83, 71);
+	noStroke();
+	fill("black");
+	rect(541, 287, 74, 50);
+	noStroke();
+	fill("#934a37");
+	rect(541, 335, 82, 20);
+}
