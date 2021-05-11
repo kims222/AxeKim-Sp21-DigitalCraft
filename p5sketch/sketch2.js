9921922b4cf7dd0dcf88f170cdd2ac4118257410
kims222 <kims222@newschool.edu>
2021-05-11 10:40:05 +0900

    sketch3

---
+++ p5sketch/sketch2.js
@@ -0,0 +1,33 @@
+function setup() {
+  createCanvas(500, 450);
+}
+
+function draw() {
+  background('#c2abf1');
+  noStroke();
+  fill('#ff7162');
+  ellipse(150, 200, 200, 50);
+  
+  fill('#ff7162');
+  ellipse(360, 200, 200, 50);
+  
+  fill('#ff7162');
+  ellipse(250, 115, 50, 200);
+  
+  fill('#ff7162');
+  ellipse(250, 310, 50, 200);
+  
+  fill('#ff7162');
+  ellipse(360, 200, 200, 50);
+  
+  fill('#edcbc8');
+  ellipse(250, 200, 70, 70);
+  
+
+
+ 
+
+
+
+
+}
---
+++ p5sketch/sketch3.js
@@ -0,0 +1,45 @@
+function setup() {
+  createCanvas(500, 400);
+}
+
+function draw() {
+  background('#d1ffc3');
+  
+  stroke('#222222');
+  fill('#eeeeee');
+  rect(150, 150, 240, 170, 10);
+  
+  noStroke();
+  fill('#222222');
+  rect(150, 180, 240, 120);
+  
+  fill(250);
+  ellipse(270, 240, 100, 100);
+  
+  fill('#949494');
+  ellipse(270, 240, 80, 80);
+  
+  fill(0);
+  ellipse(270, 240, 40, 40);
+  
+  fill(0);
+  ellipse(360, 165, 10, 10);
+  
+  fill('#dcdcdc');
+  rect(175, 128, 30, 10, 5);
+
+  fill('#949494');
+  rect(165, 135, 50, 15, 5);
+  
+
+  quad(220, 180, 320, 180, 290, 135, 250, 135);
+
+
+
+  
+
+}
+
+
+ 
+
